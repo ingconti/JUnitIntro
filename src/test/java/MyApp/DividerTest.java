06a7a0e26712969d50295cd615a5fedd295680c5
ing.conti <ingconti@ingconti.com>
2020-03-10 20:32:18 +0100

    minor fixes for JDK 13

--- src/test/java/MyApp/DividerTest.java
+++ src/test/java/MyApp/DividerTest.java
@@ -1,8 +1,10 @@
 package MyApp;
 
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DividerTest {
 
